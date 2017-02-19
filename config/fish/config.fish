f33cc6a2ba240268729c56557e0ddd4cb1a7755e
Aldo Ridhoni <aldoridhoni@gmail.com>
2017-02-19 17:25:53 +0700

    rust

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,6 +5,18 @@ if test -e '/usr/local/textlive/2015/bin/x86_64-linux/tex'
   set fish_user_paths $fish_user_paths '/usr/local/texlive/2015/bin/x86_64-linux'
 end
 
+# Rust
+if test -e ~/.cargo/bin
+  set fish_user_paths $fish_user_paths ~/.cargo/bin
+end
+
+if test -e ~/.multirust/toolchains
+  set toolchain (rustup show | grep '(default)' | awk '{print $1}')
+  if test -e ~/.multirust/toolchains/$toolchain/lib/rustlib/src/rust/src
+    set -x RUST_SRC_PATH ~/.multirust/toolchains/$toolchain/lib/rustlib/src/rust/src
+  end
+end
+
 # /usr/local/bin
 set fish_user_paths $fish_user_paths '/usr/local/bin' '/usr/sbin' '/sbin' '/usr/bin' '/usr/sbin'
 
