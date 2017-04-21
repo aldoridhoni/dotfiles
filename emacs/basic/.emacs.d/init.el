6fceda47c176427089472c87d0320e449d511c62
Aldo Ridhoni <aldoridhoni@gmail.com>
2017-04-22 00:30:56 +0700

    melpa packages

--- emacs/basic/.emacs.d/init.el
+++ emacs/basic/.emacs.d/init.el
@@ -3,6 +3,9 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (custom-set-variables
@@ -15,6 +18,9 @@
  '(global-linum-mode t)
  '(global-reveal-mode t)
  '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (helm-descbinds helm-projectile helm blank-mode web-mode)))
  '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -22,3 +28,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'helm-config)
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+(setq helm-descbinds-window-style 'split
+      helm-ff-file-name-history-use-recentf t)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
