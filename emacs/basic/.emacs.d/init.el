b6b79a38e3d2174ceb93c78d3faa16ed000cbf24
Aldo Ridhoni <aldoridhoni@gmail.com>
2017-04-29 11:57:59 +0700

    Cleanup

--- emacs/basic/.emacs.d/init.el
+++ emacs/basic/.emacs.d/init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(setq package-list '(helm helm-descbinds which-key blank-mode web-mode))
+(setq package-list '(helm helm-descbinds which-key blank-mode web-mode monokai-theme))
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -16,16 +16,34 @@
 
 
 (custom-set-variables
- '(custom-enabled-themes (quote (leuven)))
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" default)))
  '(global-hl-line-mode t)
  '(global-linum-mode t)
  '(global-reveal-mode t)
  '(inhibit-startup-screen t)
- '(menu-bar-mode t)
- '(tool-bar-mode nil)
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (monokai-theme which-key web-mode helm-projectile helm-descbinds blank-mode)))
  '(visible-bell t))
 (custom-set-faces
  )
+
+;; Do something if using X
+(when (display-graphic-p)
+  ;; Hide tool-bar
+  (tool-bar-mode 0)
+  ;; Toggle menu-bar
+  (menu-bar-mode)
+  ;; Set leuven theme
+  (load-theme 'leuven)
+  (enable-theme 'leuven)
+  ;; Maximize initial frame
+  (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+)
 
 ;; Helm Package
 (require 'helm-config)
@@ -40,10 +58,7 @@
 (setq which-key-popup-type 'minibuffer)
 (setq which-key-idle-delay 0.4)
 
-;; Keybind
+;; Keybinds
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-;; Maximize initial frame
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
