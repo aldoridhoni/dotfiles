364a96291072b640b1ae49355475c6ff1b3731b8
Aldo Ridhoni <aldoridhoni@gmail.com>
2017-09-19 21:14:51 +0700

    Deleting folder

--- emacs/basic/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# https://stackoverflow.com/questions/29832037/how-to-get-script-directory-in-posix-sh
-
-dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
-env HOME="$dir" REAL_HOME="$HOME" emacs --maximized $@
