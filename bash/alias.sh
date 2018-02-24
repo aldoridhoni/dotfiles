116b09faf03c5001dd1a0e87742f84ce0e6f3e2e
Aldo Ridhoni <aldoridhoni@gmail.com>
2018-02-24 15:07:11 +0700

    change quotes

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-alias pacup="sudo pacman -Syu"
-alias top="htop"
-alias mkdir="mkdir -pv"
-alias wget="wget -c"
-alias c="tput clear"
-alias py="env python"
-alias py3="env python3"
-alias df="df -h -T -l -x tmpfs -x devtmpfs"
-alias free="free -h"
-alias psu="ps -u $USER -o pid,args"
-alias rm="rm -i"
-alias b="c; tput cup $(tput lines)"
-alias ll="ls -l"
-alias la="ls -al"
+alias pacup='sudo pacman -Syu'
+alias top='htop'
+alias mkdir='mkdir -pv'
+alias wget='wget -c'
+alias c='tput clear'
+alias py='env python'
+alias py3='env python3'
+alias df='df -h -T -l -x tmpfs -x devtmpfs'
+alias free='free -h'
+alias psu='ps -u $USER -o pid,args'
+alias rm='rm -i'
+alias b='c; tput cup $(tput lines)'
+alias ll='ls -l'
+alias la='ls -al'
